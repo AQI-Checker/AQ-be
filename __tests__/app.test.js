603a848f29dbbe1df2178533d48969df41f809f1
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-09-16 15:41:34 -0700

    added locationQueue w/ test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,19 @@
-const fs = require('fs');
-const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('AQ-be routes', () => {
-  beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+describe('AQ email routes', () => {
+  it('POST route that takes a user location and phone number', async() => {
+    const response = await request(app)
+      .post('/api/v1/air-quality')
+      .send({
+        userPhoneNumber:'240-565-9921',
+        userLocation: 'Portland, Oregon, US'
+      });
+
+    expect(response.body).toEqual({
+      jobId: expect.any(String),
+      userPhoneNumber:'240-565-9921',
+      userLocation: 'Portland, Oregon, US'
+    });
   });
 });
