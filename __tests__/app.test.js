0f4ed2aec4f9c5a1e2fbb91d5cd0197c5e4063c9
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-09-16 17:48:21 -0700

    add GET endpoint with test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,11 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const { locationQueue } = require('../lib/pipeline/queue');
 
 describe('AQ email routes', () => {
+  afterAll(() => {
+    return locationQueue.close();
+  })
   it('POST route that takes a user location and phone number', async() => {
     const response = await request(app)
       .post('/api/v1/air-quality')
@@ -16,4 +20,12 @@ describe('AQ email routes', () => {
       userLocation: 'Portland, Oregon, US'
     });
   });
+
+  it('GET route that returns the AQI for the userLocation', async() => {
+    const response = await request(app)
+      .get('/api/v1/current?city=portland&state=or&country=us')
+
+    console.log('AQI', response.body)
+    expect(response.body).toEqual(expect.any(Number));
+  })
 });
